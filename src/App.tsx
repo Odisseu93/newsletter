cbeffe225fbe56957b4c7af7bb34279783d63b56
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-17 15:23:36 -0300

    chore: change `semi` rule in eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-	return <h1 className='text-4xl font-bold'>Stay Upadated!</h1>;
+	return (
+		<h1 className='text-4xl font-bold text-neutral-charcoal-grey'>
+			Stay Upadated!
+		</h1>
+	)
 }
 
-export default App;
+export default App
