b0a6048d5a4bdd6730f33d0b21314af03e34911c
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-19 17:30:48 -0300

    fix: add `/success` and organize the other routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Outlet,
+} from 'react-router-dom'
 
 import { Subscribe } from './pages/Subscribe'
+import { Success } from './pages/Success'
 
 const App = () => (
 	<Router>
 		<Routes>
 			<Route path='/' element={<Subscribe />} />
-			<Route path='/subscribe' element={<Subscribe />} />
+			<Route path='subscribe' element={<Outlet />}>
+				<Route index element={<Subscribe />} />
+				<Route path='success' element={<Success />} />
+			</Route>
 		</Routes>
 	</Router>
 )
