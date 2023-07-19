1b68ba1f41e0cd40cd0133b8093903b0924451c8
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-19 11:17:57 -0300

    feat: implement route "/" and "/subscribe"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { Subscribe } from './pages/Subscribe'
 
-const App= () => <Subscribe />
+const App = () => (
+	<Router>
+		<Routes>
+			<Route path='/' element={<Subscribe />} />
+			<Route path='/subscribe' element={<Subscribe />} />
+		</Routes>
+	</Router>
+)
 
 export default App
