356ea4c81ae0a11b3af360cc12be81fcbc895487
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-20 11:41:55 -0300

    fix: adjust responsiveness

--- src/templates/SubscribeTemplate/styles.ts
+++ src/templates/SubscribeTemplate/styles.ts
@@ -1,5 +1,6 @@
 export const className = {
-	main: 'flex flex-col content-center desktop:w-[800px] desktop:mx-auto desktop:my-5 desktop:flex-row mb-28',
+	main: 'flex flex-col content-center max-w-[100vw] overflow-x-hidden' +
+		'desktop:w-[800px] desktop:mx-auto desktop:flex-row desktop:h-[calc(100vh-56px)]',
 	picture: 'mx-auto w-max desktop:order-2',
 	section: 'flex flex-col gap-4 p-4 desktop:order-1 min-[413px]:mx-auto'
 }
